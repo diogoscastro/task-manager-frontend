1b772ae6fef59babd29d3d5f103ab8b702646f1e
diogoscatro <diogocas89@gmail.com>
2025-11-17 12:05:13 -0300

    feat: add react-router-dom setup and home page

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Bounce, ToastContainer } from "react-toastify";
+import { BrowserRouter } from "react-router-dom";
 
 import "./index.scss";
 import reportWebVitals from "./reportWebVitals";
@@ -9,22 +10,24 @@ import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    <React.StrictMode>
-        <ToastContainer
-            position="top-center"
-            autoClose={3000}
-            hideProgressBar={false}
-            newestOnTop={false}
-            closeOnClick={false}
-            rtl={false}
-            pauseOnFocusLoss
-            pauseOnHover
-            draggable
-            theme="dark"
-            transition={Bounce}
-        />
-        <App />
-    </React.StrictMode>
+    <BrowserRouter>
+        <React.StrictMode>
+            <ToastContainer
+                position="top-center"
+                autoClose={3000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick={false}
+                rtl={false}
+                pauseOnFocusLoss
+                pauseOnHover
+                draggable
+                theme="dark"
+                transition={Bounce}
+            />
+            <App />
+        </React.StrictMode>
+    </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
