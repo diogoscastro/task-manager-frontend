5ec58f433421c1df80e37fda5d77454c4a5704b6
diogoscatro <diogocas89@gmail.com>
2025-11-25 15:39:27 -0300

    feat: add params to home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,17 @@
+import { useParams } from "react-router-dom";
+import { useEffect } from "react";
+
 import Sidebar from "../components/Sidebar";
 import Tasks from "../components/Tasks";
 
 import "./Home.scss";
 
 const Home = () => {
+    const params = useParams();
+
+    useEffect(() => {
+        console.log({ params });
+    }, [params]);
     return (
         <div className="home-container">
             <Sidebar />
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -9,7 +9,7 @@ const Login = () => {
     const navigate = useNavigate();
 
     const handleSignInClick = () => {
-        navigate("/");
+        navigate("/Diogo");
     };
     return (
         <div className="login-container">
